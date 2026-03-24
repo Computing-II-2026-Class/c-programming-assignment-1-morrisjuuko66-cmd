038ff755be0bfd37a902408aed5bb6a3375bef48
morrisjuuko66-cmd <morrisjuuko66@gmail.com>
2026-03-24 20:17:21 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -5,7 +5,27 @@
 
 int main(void)
 {
-    /* Write your code here */
+loat unitsConsumed, costPerunit, totalElectricityBill;
+    // Prompt the user for the units consumed and the cost per unit
+
+
+    printf("Enter the units consumed: ");
+    scanf("%f", &unitsConsumed);
+    printf("Enter the cost per unit: ");
+    scanf("%f", &costPerunit);
+
+    // Calculate the total electricity bill
+
+    totalElectricityBill = unitsConsumed * costPerunit;
+
+    // Display the total electricity bill
+
+    printf("The total electricity bill is: %.2f UGX\n", totalElectricityBill);
+    printf("..........Electricity Bill Details..........\n");
+    printf("The units consumed are: %.2f\n", unitsConsumed);
+    printf("The cost per unit is: %.2f UGX\n", costPerunit);
+    printf("\nThank you for using our service!");
+
 
     return 0;
 }
